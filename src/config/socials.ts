fb9e51727fe599c71e756da21f3116ed1e758e0d
u/Horror_Amphibian7516 <nuitsukera@vk.com>
2025-08-23 22:47:48 -0300

    1.0.5

--- src/config/socials.ts
+++ src/config/socials.ts
@@ -1,6 +1,5 @@
 export const SOCIALS = {
-  spotify:
-    'https://steamcommunity.com/profiles/76561199097964159',
+  spotify: 'https://steamcommunity.com/profiles/76561199097964159',
   instagram: 'https://instagram.com/hikkikomari__/',
   reddit: 'https://reddit.com/user/KanniNero/',
   steam: 'https://steamcommunity.com/profiles/76561199097964159',
